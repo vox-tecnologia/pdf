087c804a18c2f2cd206149ca489aa33abd5a498b
Philipe Fernandes <phlllpe@users.noreply.github.com>
2018-08-09 13:58:45 -0300

    Update PdfInterface.php

--- src/Pdf/PdfInterface.php
+++ src/Pdf/PdfInterface.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
- * This file is part of the UCSDMath package.
- *
- * (c) 2015-2017 UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- * @author Daryl Eisner <deisner@ucsd.edu>
- */
-
 declare(strict_types=1);
 
 namespace UCSDMath\Pdf;
@@ -19,7 +9,6 @@ namespace UCSDMath\Pdf;
  *
  * Method list: (+) @api.
  *
- * @author Daryl Eisner <deisner@ucsd.edu>
  *
  * @api
  */
