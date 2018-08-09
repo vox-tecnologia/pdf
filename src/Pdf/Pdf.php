ef73478f18d2fa1e974fe6a5cd840f34f1cb9f76
Philipe Fernandes <phlllpe@users.noreply.github.com>
2018-08-09 13:58:26 -0300

    Update Pdf.php

--- src/Pdf/Pdf.php
+++ src/Pdf/Pdf.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
- * This file is part of the UCSDMath package.
- *
- * (c) 2015-2017 UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- * @author Daryl Eisner <deisner@ucsd.edu>
- */
-
 declare(strict_types=1);
 
 namespace UCSDMath\Pdf;
@@ -33,7 +23,6 @@ namespace UCSDMath\Pdf;
  * (+) PdfInterface setOutputDestination(string $destination);
  * (+) PdfInterface initializePageSetup(string $pageSize = null, string $orientation = null);
  *
- * @author Daryl Eisner <deisner@ucsd.edu>
  */
 class Pdf extends AbstractPdfAdapter implements PdfInterface
 {
